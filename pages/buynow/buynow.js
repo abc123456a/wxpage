9e97aaff198e6450787c46b9c59df9546597eca1
abc123456a <15101604016@163.com>
2017-04-03 18:03:01 +0800

    xinxi

---
+++ pages/buynow/buynow.js
@@ -0,0 +1,59 @@
+//index.js
+//获取应用实例
+var app = getApp()
+var order = ['demo1', 'demo2', 'demo3']
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    toView: 'green'
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  },
+   upper: function(e) {
+    console.log(e)
+  },
+  lower: function(e) {
+    console.log(e)
+  },
+  scroll: function(e) {
+    console.log(e)
+  },
+  scrollToTop: function(e) {
+    this.setAction({
+      scrollTop: 0
+    })
+  },
+  tap: function(e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1],
+          scrollTop: (i + 1) * 200
+        })
+        break
+      }
+    }
+  },
+  tapMove: function(e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
+  }
+})
+
