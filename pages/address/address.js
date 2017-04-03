ad56c0ce8e1a6618ee3cabd7ecf5af182c466114
boxser <1137089297@qq.com>
2017-04-03 22:06:45 +0800

    new

--- pages/address/address.js
+++ pages/address/address.js
@@ -1,59 +1,27 @@
-//index.js
-//获取应用实例
-var app = getApp()
-var order = ['demo1', 'demo2', 'demo3']
+var util = require('../../utils/util.js')
+var formatLocation = util.formatLocation
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
-    toView: 'green'
+    hasLocation: false
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
+  getLocation: function () {
     var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  },
-   upper: function(e) {
-    console.log(e)
-  },
-  lower: function(e) {
-    console.log(e)
-  },
-  scroll: function(e) {
-    console.log(e)
-  },
-  scrollToTop: function(e) {
-    this.setAction({
-      scrollTop: 0
-    })
-  },
-  tap: function(e) {
-    for (var i = 0; i < order.length; ++i) {
-      if (order[i] === this.data.toView) {
-        this.setData({
-          toView: order[i + 1],
-          scrollTop: (i + 1) * 200
+    wx.getLocation({
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          hasLocation: true,
+          location: formatLocation(res.longitude, res.latitude)
         })
-        break
       }
-    }
+    })
   },
-  tapMove: function(e) {
+  clear: function () {
     this.setData({
-      scrollTop: this.data.scrollTop + 10
+      hasLocation: false
     })
   }
 })
+
 
--- utils/util.js
+++ utils/util.js
@@ -16,6 +16,22 @@ function formatNumber(n) {
   return n[1] ? n : '0' + n
 }
 
+function formatLocation(longitude, latitude) {
+  if (typeof longitude === 'string' && typeof latitude === 'string') {
+    longitude = parseFloat(longitude)
+    latitude = parseFloat(latitude)
+  }
+
+  longitude = longitude.toFixed(2)
+  latitude = latitude.toFixed(2)
+
+  return {
+    longitude: longitude.toString().split('.'),
+    latitude: latitude.toString().split('.')
+  }
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  formatLocation: formatLocation
 }
